56b86c81a37b1dde8954c09d9291d20eb2d54fb7
Baraa963 <127866968+Baraa963@users.noreply.github.com>
2025-04-04 11:24:17 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <p className="read-the-docs">
+        Click ooon the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
